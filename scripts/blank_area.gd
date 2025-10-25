9a9a69e5df4c755fff1610750f56502a5a6cf989
Ash <148392742+ashilated@users.noreply.github.com>
2025-10-25 14:27:47 -0700

    added task button for wip areas

--- scripts/blank_area.gd
+++ scripts/blank_area.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
 func _on_button_pressed() -> void:
+	$Button.disabled = true
+	$Button.visible = false
+	$TaskMarkerButton.visible = true
 	# open the task creation gui
-	pass
+
+
+func _on_task_marker_button_mouse_entered() -> void:
+	$TaskMarkerButton.position.y -= 1
+
+
+func _on_task_marker_button_mouse_exited() -> void:
+	$TaskMarkerButton.position.y += 1
+
+
+func _on_task_marker_button_pressed() -> void:
+	# open task view gui
+	pass # Replace with function body.
